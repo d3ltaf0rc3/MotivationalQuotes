4547cece1fcffe970323677b0f5539a5c2a543c2
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-09-24 21:00:25 +0300

    feat: display quotes

---
+++ src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+  <p class="quote">
+    "{{ quote }}"
+  </p>
+</template>
+
+<script>
+import quotes from "../assets/quotes.json";
+
+export default {
+  name: "Quote",
+  data() {
+    return {
+      quote: quotes[parseInt(Math.random() * (quotes.length - 0) + 0)],
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+.quote {
+  color: white;
+  font-size: 48px;
+  font-family: "Montserrat", sans-serif;
+  font-style: italic;
+  text-align: center;
+  word-break: break-word;
+}
+</style>
--- src/components/QuoteBox.vue
+++ src/components/QuoteBox.vue
@@ -1,21 +1,29 @@
 <template>
     <div class="container">
-
+        <Quote />
     </div>
 </template>
 
 <script>
+import Quote from './Quote.vue';
+
 export default {
-    name: "QuoteBox"
+    name: "QuoteBox",
+    components: {
+        Quote
+    }
 }
 </script>
 
 <style scoped>
     .container {
-        height: 50vh;
+        height: 60vh;
         width: 60vw;
         background-color: rgba(155, 155, 155, 0.8);
         border-radius: 15px;
         box-shadow: 1px 1px #000;
+        padding: 50px;
+        display: grid;
+        place-items: center;
     }
 </style>
